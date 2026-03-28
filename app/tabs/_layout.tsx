cb4843ffbc6f15134b3c62bbfa4322417ad46251
CaioAlexanderx <caio.pantoja@gmail.com>
2026-03-28 16:03:43 -0300

    Create _layout.tsx

---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,15 @@
+import { Tabs } from "expo-router";
+import { Colors } from "@/constants/colors";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{
+      headerShown: false,
+      tabBarStyle: { backgroundColor: Colors.bg2, borderTopColor: Colors.border },
+      tabBarActiveTintColor: Colors.violet3,
+      tabBarInactiveTintColor: Colors.ink3,
+    }}>
+      <Tabs.Screen name="index" options={{ title: "Painel" }} />
+    </Tabs>
+  );
+}
