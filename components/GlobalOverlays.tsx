8d7f46b5034233a569c34318f3c375ac5c7ecafa
CaioAlexanderx <caio.pantoja@gmail.com>
2026-04-18 01:14:14 -0300

    feat: GlobalOverlays wrapper para ChatFAB
    Wrapper leve para facilitar integracao do FAB no _layout
    sem precisar editar o arquivo de 20KB diretamente.

---
+++ components/GlobalOverlays.tsx
@@ -0,0 +1,7 @@
+// Wrapper que renderiza o ChatFAB globalmente em todas as tabs
+// Import e use: <GlobalOverlays /> em qualquer ponto do layout
+import { ChatFAB } from "@/components/ChatFAB";
+
+export function GlobalOverlays() {
+  return <ChatFAB />;
+}
